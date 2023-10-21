bd1f7fdf56426792f818e641d04a5f4ca5178e1d
ILoveHumanity <silver-wind@inbox.ru>
2023-10-21 18:30:06 +0700

    пофиксил вывод булева вектора

--- code_Huffman.cpp
+++ code_Huffman.cpp
@@ -1,21 +1,17 @@
 ﻿// code_Haphman.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-
-
 #include <iostream>
 #include <vector>
 #include <string>
-
 struct Bool_vector {
     int value = 0; int size = 0;
 };
 std::ostream& operator << (std::ostream& os, const Bool_vector& a) {
-    for (int i = a.size; i >= 0; --i) {
+    for (int i = a.size - 1; i >= 0; --i) {
         if (a.value & (1 << i)) { os << '1'; }
         else { os << '0'; }
     }
     return os;
 };
-
 void get_code(std::vector<Bool_vector>& code, int local_len) {
     if (local_len <= 2) {
         for (int i = 0; i < local_len; ++i) {
@@ -25,7 +21,7 @@ void get_code(std::vector<Bool_vector>& code, int local_len) {
         return;
     }
     int t = code[local_len - 1].value + code[local_len - 2].value, j = local_len - 2, s;
-    while (j > 0 && t > code[j - 1].value) {
+    while (j > 0 && t >= code[j - 1].value) { // >= для красивого результата)
         code[j] = code[j - 1];
         --j;
     }
@@ -43,8 +39,6 @@ void get_code(std::vector<Bool_vector>& code, int local_len) {
     code[j + 1].size = s + 1;
     return;
 }
-
-
 int main()
 {
     std::string text;
@@ -55,7 +49,6 @@ int main()
     }
     std::vector<int> probability;
     std::vector<char> signs;
-
     for (int i = 0; i < (int)encrypting_table.size(); ++i) {
         std::cout << encrypting_table[i] << ' ';
         if (encrypting_table[i]) {
@@ -71,32 +64,23 @@ int main()
             signs[j] = i;
         }
     }
-
-
     std::vector<Bool_vector> code((int)probability.size());
-
     for (int i = 0; i < (int)probability.size(); ++i) {
         std::cout << '\n' << probability[i] << '\t' << signs[i];
         code[i].value = probability[i];
     }
 
-
-
     get_code(code, (int)code.size());
     std::cout << '\n';
-
     for (int i = 0; i < (int)code.size(); ++i) {
         //вставить заполнение кодами encrypting_table(для удобства шифрования)
         std::cout << '\n' << code[i] << '\t' << signs[i];
     }
-
     /*for(int i=0; i < (int)encrypting_table.size(); ++i){
         std::cout << encrypting_table[i] << ' ';
     }*/
     std::cout << '\n' << text;
-
     return 0;
 }
-
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
